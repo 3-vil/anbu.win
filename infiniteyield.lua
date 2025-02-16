0c9c94743cb0685bc05afd610fd41ebcd86e6632
3-vil <john@e-vil.com>
2025-02-16 16:29:17 -0600

    Update infiniteyield.lua

--- infiniteyield.lua
+++ infiniteyield.lua
@@ -611,7 +611,7 @@ Logo.BackgroundTransparency = 1
 Logo.BorderSizePixel = 0
 Logo.Position = UDim2.new(0, 125, 0, 127)
 Logo.Size = UDim2.new(0, 10, 0, 10)
-Logo.Image = "rbxassetid://1352543873"
+Logo.Image = "rbxassetid://2499469079"
 Logo.ImageTransparency = 0
 Logo.ZIndex = 10
 
@@ -623,7 +623,7 @@ Credits.Position = UDim2.new(0, 0, 0.9, 30)
 Credits.Size = UDim2.new(0, 250, 0, 20)
 Credits.Font = Enum.Font.SourceSansLight
 Credits.FontSize = Enum.FontSize.Size18
-Credits.Text = "Edge // Zwolf // Moon // Toon"
+Credits.Text = "Anbu.win"
 Credits.TextColor3 = Color3.new(1, 1, 1)
 Credits.ZIndex = 10
 
